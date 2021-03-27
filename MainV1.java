ada08e629d0b01c8950c458a87ba0ef9487fb388
Maksim <maksimshalaev11@gmail.com>
2021-03-27 17:40:34 -0600

    Change 1

---
+++ MainV1.java
@@ -0,0 +1,8 @@
+public class MainV1 {
+    public static void main(String[] args) {
+        int price = (int) 94257.81F;
+        int spent = 20;
+        int miles = price / spent;
+        System.out.println("Total: " + miles);
+    }
+}
